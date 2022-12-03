255c417bb9649763dc08b30d2f003aed92f2f8e9
Keison <keison-tang@users.noreply.github.com>
2022-12-03 21:53:53 +1300

    add template

---
+++ template.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+function ProcessLine(line) {
+  if (line == '') return;
+
+  console.log(line);
+}
+
+try {
+  // read contents of the file
+  const data = fs.readFileSync(__dirname + '/input.txt', 'UTF-8');
+
+  // split the contents by new line
+  const lines = data.split(/\r?\n/);
+
+  // print all lines
+  lines.forEach((line) => {
+    ProcessLine(line);
+  });
+} catch (err) {
+  console.error(err);
+}
